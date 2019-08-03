c062facf8959c933eab2ce0a501a293c79af425a
Renee Sims <info@reneesoul.com>
2019-08-03 07:08:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,10 +45,7 @@ end
 
 def sum_array(array)
   new_array = []
-  new_array = array.inject {|sum, x| sum + x } 
-    new_array
-  end
-  z
+  array.inject {|sum, x| sum + x new_array << sum}
 end
 
 
