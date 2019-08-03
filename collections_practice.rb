350334d7089bdc722800f9407d5fca3477a099ce
Renee Sims <info@reneesoul.com>
2019-08-03 06:15:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
 
+
+def sort_array_asc(num)
+  copy = num.sort
+  
+end
+
+
+def sort_array_desc(array)
+  reverse = array.sort! {|num1, num2| num2 <=> num1}
+end
+
+
+def sort_array_char_count(array)
+  char_count = array.sort {|x, y| x.length <=> y.length}
+end
+
+
+def swap_elements(array)
+  new_array = array.sort! {|elmnt1, elmnt2| elmnt1[1] <=> elmnt2[2]}
+  new_array
+end
+
+
+def reverse_array(nums)
+  new_array = nums.reverse! 
+  
+end
+
+
+def kesha_maker(array)
+  new_array = array.each do |x| 
+    x.split("")
+    x[2] = "$"
+  end
+end
+
+
+def find_a(array)
+  array.collect {|x| x.start_with?("a") return x}
+  
+end
+
+
+def sum_array
+  
+end
+
+
+def add_s
+  
+end
