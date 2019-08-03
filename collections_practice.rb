1a15cf01b2a7dcd5a1e9b4288c17aebd6850a0b4
Renee Sims <info@reneesoul.com>
2019-08-03 08:02:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,7 +50,7 @@ end
 
 
 def add_s(array)
-  array.each_with_index.collect {|elmnt1, index| "#{elmnt1}s" if index != 2}
+  new_array = array.each_with_index.collect {|elmnt1, index| "#{elmnt1}s" if index != 2}
 
-    array
+  
 end
